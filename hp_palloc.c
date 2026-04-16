c4fd76aaa2f22e0d2034e7be05370e02b4c4c45c
syhhyl <syhhyl926@126.com>
2026-04-16 11:16:29 +0800

    update

--- hp_palloc.c
+++ hp_palloc.c
@@ -2,6 +2,235 @@
  * Copyright (C) Sun Yuhang
  */
  
- #include "hp_palloc.h"
+#include <complex.h>
+#include <stdlib.h>
+#include "hp_palloc.h"
  
- 
+static inline void *
+hp_memalign(size_t alignment, size_t size) {
+  void *p;
+  if (posix_memalign(&p, alignment, size) != 0) return NULL;
+  
+  return p;
+}
+
+static inline void
+hp_free(void *p) {
+  free(p);
+}
+
+hp_pool_t *hp_create_pool(size_t size) {
+  hp_pool_t *p;   
+  
+  p = hp_memalign(HP_POOL_ALIGNMENT, size);
+  if (p == NULL) return NULL;
+  
+  p->d.last = (u_char *)p + sizeof(hp_pool_data_t);
+  p->d.end = (u_char *)p + size;
+  p->d.next = NULL;
+  p->d.failed = 0;
+  
+  size = size - sizeof(hp_pool_t);
+  p->max = (size < HP_MAX_ALLOC_FROM_POOL) ? size : HP_MAX_ALLOC_FROM_POOL; 
+  
+  p->current = p;
+  p->large = NULL;
+  p->cleanup = NULL;
+
+  return p;
+}
+
+
+
+void hp_destroy_pool(hp_pool_t *pool) {
+  
+  hp_pool_t *p, *n;
+  hp_pool_large_t *l;
+  hp_pool_cleanup_t *c;
+  
+  for (c = pool->cleanup; c; c = c->next) {
+    if (c->handler) {
+      c->handler(c->data);
+    }
+  }
+  
+  for (l = pool->large; l; l = l->next) {
+    if (l->alloc) {
+      hp_free(l->alloc);
+    }
+  }
+  
+  for (p = pool, n = pool->d.next; ; p = n, n = n->d.next) {
+    hp_free(p);
+    if (n == NULL) break;
+  }
+}
+
+void hp_reset_pool(hp_pool_t *pool) {
+  hp_pool_t *p;
+  hp_pool_large_t *l;
+  
+  for (l = pool->large; l; l = l->next) {
+    if (l->alloc) {
+      hp_free(l->alloc);
+    }
+  }
+  
+  for (p = pool; p; p = p->d.next) {
+    p->d.last = (u_char *)p + sizeof(hp_pool_t);
+    p->d.failed = 0;
+  }
+  
+  pool->current = pool;
+  pool->large = NULL;
+}
+
+static void *
+hp_palloc_block(hp_pool_t *pool, size_t size) {
+  u_char *m;
+  size_t psize;
+  hp_pool_t *p, *new;
+  
+  psize = (size_t) (pool->d.end - (u_char *)pool);
+  m = hp_memalign(HP_POOL_ALIGNMENT, psize);
+  if (m == NULL) {
+    return NULL;
+  }
+  
+  new = (hp_pool_t *)m;
+  new->d.end = m + psize;
+  new->d.next = NULL;
+  new->d.failed = 0;
+  
+  m += sizeof(hp_pool_data_t);
+  m = hp_align_ptr(m, HP_ALIGNMENT);
+  new->d.last = m + size;
+  
+  for (p = pool->current; p->d.next; p = p->d.next) {
+    if (p->d.failed++ > 4) {
+      pool->current = p->d.next;
+    }
+  }
+  p->d.next = new;
+  return m;
+}
+
+static inline void *
+hp_palloc_small(hp_pool_t *pool, size_t size, hp_uint_t align) {
+  u_char *m;
+  hp_pool_t *p;
+  
+  p = pool->current;
+  
+  do {
+    m = p->d.last;
+    if (align) {
+      m = hp_align_ptr(m, HP_ALIGNMENT);
+    }
+    
+    if ((size_t)(p->d.end - m) >= size) {
+      p->d.last = m + size;
+      return m;
+    }
+    
+    p = p->d.next;
+  } while (p);
+
+  return hp_palloc_block(pool, size);
+}
+
+
+
+static inline void *
+hp_alloc(size_t size) {
+  return malloc(size);
+}
+
+static void *
+hp_palloc_large(hp_pool_t *pool, size_t size) {
+  void *p;
+  hp_uint_t n;
+  hp_pool_large_t *large;
+  
+  p = hp_alloc(size);
+  if (p == NULL) return NULL;
+
+  n = 0;
+  for (large = pool->large; large; large = large->next) {
+    if (large->alloc == NULL) {
+      large->alloc = p;
+      return p;
+    }
+    
+    if (n++ > 3) break;
+  }
+  
+  large = hp_palloc_small(pool, sizeof(hp_pool_large_t), 1);
+  if (large == NULL) {
+    hp_free(p);
+    return NULL;
+  }
+  
+  large->alloc = p;
+  large->next = pool->large;
+  pool->large = large;
+  
+  return p;
+}
+
+void *
+hp_pmemalign(hp_pool_t *pool, size_t size, size_t alignment)
+{
+    void              *p;
+    hp_pool_large_t  *large;
+
+    p = hp_memalign(alignment, size);
+    if (p == NULL) {
+        return NULL;
+    }
+
+    large = hp_palloc_small(pool, sizeof(hp_pool_large_t), 1);
+    if (large == NULL) {
+        hp_free(p);
+        return NULL;
+    }
+
+    large->alloc = p;
+    large->next = pool->large;
+    pool->large = large;
+
+    return p;
+}
+
+void *hp_palloc(hp_pool_t *pool, size_t size) {
+  if (size <= pool->max) {
+    return hp_palloc_small(pool, size, 1);
+  } 
+  
+  return hp_palloc_large(pool, size);
+}
+
+void *hp_pnalloc(hp_pool_t *pool, size_t size) {
+  if (size <= pool->max) {
+    return hp_palloc_small(pool, size, 0);
+  } 
+  
+  return hp_palloc_large(pool, size);
+}
+
+hp_int_t
+hp_pfree(hp_pool_t *pool, void *p)
+{
+    hp_pool_large_t  *l;
+
+    for (l = pool->large; l; l = l->next) {
+        if (p == l->alloc) {
+            hp_free(l->alloc);
+            l->alloc = NULL;
+
+            return HP_OK;
+        }
+    }
+
+    return HP_DECLINED;
+}
--- hp_palloc.h
+++ hp_palloc.h
@@ -6,23 +6,70 @@
 #define _HP_PALLOC_H_INCLUDED_
 
 #include <stddef.h>
+#include <stdint.h>
 
+#define HP_MAX_ALLOC_FROM_POOL (4096 - 1)
+#define HP_POOL_ALIGNMENT 16
+#define hp_align_ptr(d, a) \
+  (u_char *) (((uintptr_t)(p) + ((uintptr_t)(a)-1)) & ~((uintptr_t)(a)-1)) 
 
+#define HP_ALIGNMENT sizeof(uintptr_t)
+#define HP_OK 0
+#define HP_DECLINED -5
+
+typedef unsigned char u_char;
+typedef struct hp_pool_s hp_pool_t;
+typedef uintptr_t hp_uint_t;
+typedef intptr_t hp_int_t;
+typedef void (*hp_pool_cleanup_pt)(void *data);
+typedef struct hp_pool_cleanup_s hp_pool_cleanup_t;
 typedef struct hp_pool_large_s hp_pool_large_t;
+typedef int hp_fd_t;
+
+
 struct hp_pool_large_s {
   hp_pool_large_t *next;
   void *alloc;
 };
 
-typedef struct hp_pool_s hp_pool_t;
+typedef struct {
+    u_char *last;
+    u_char *end;
+    hp_pool_t *next;
+    hp_uint_t failed;
+} hp_pool_data_t;
+
+struct hp_pool_cleanup_s {
+  hp_pool_cleanup_pt handler;
+  hp_pool_cleanup_t *next;
+  void *data;
+};
+
 struct hp_pool_s{
-  
+  hp_pool_data_t d;
   size_t max;
   hp_pool_t *current;
   hp_pool_large_t *large;
-  
-   
+  hp_pool_cleanup_t *cleanup;
 };
+
+
+
+typedef struct {
+  hp_fd_t fd;
+  u_char *name;
+} hp_pool_cleanup_file_t;
+
+
+
+hp_pool_t *hp_create_pool(size_t size);
+void hp_destroy_pool(hp_pool_t *pool);
+
+void *hp_palloc(hp_pool_t *pool, size_t size);
+void *hp_pnalloc(hp_pool_t *pool, size_t size);
+
+
+
 
 #endif
  
